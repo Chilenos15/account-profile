9424e0d1219923bfc4a75a6964f12be6995af7b1
Freddy Ramos <Freddy Ramos>
2021-12-21 20:22:05 -0700

    finished styling

--- client/src/components/users/Profile.js
+++ client/src/components/users/Profile.js
@@ -1,12 +1,13 @@
 import Account from './Account';
 import AccountForm from './AccountForm';
+import { MainContainer } from '../../styles/profileStyles';
 
 const Profile = ({}) => {
   return (
-    <>
+    <MainContainer>
       <Account />
       <AccountForm />
-    </>
+    </MainContainer>
   )
 }
 
